3e60addb13ebc3dc70e6908f9edcd5806e6ca128
= <=>
2024-12-15 20:59:57 +0700

    PongGame

--- PongGame.py
+++ PongGame.py
@@ -36,44 +36,42 @@ ball1.shape("square")
 ball1.color("green")
 ball1.penup()
 ball1.goto(0, 0)
-ball1.dx = 2
-ball1.dy = -2
-ball1.time = -1000
+ball1.dx = 0.10
+ball1.dy = -0.10
 
-# Ball
-ball2 = turtle.Turtle()
-ball2.speed(0)
-ball2.shape("square")
-ball2.color("blue")
-ball2.penup()
-ball2.goto(0, 0)
-ball2.dx = -2
-ball2.dy = -2
-ball1.time = -1000
+# # Ball
+# ball2 = turtle.Turtle()
+# ball2.speed(0)
+# ball2.shape("square")
+# ball2.color("blue")
+# ball2.penup()
+# ball2.goto(0, 0)
+# ball2.dx = -0.10
+# ball2.dy = -0.10
 
-# Ball
-ball3 = turtle.Turtle()
-ball3.speed(0)
-ball3.shape("square")
-ball3.color("yellow")
-ball3.penup()
-ball3.goto(0, 0)
-ball3.dx = 1
-ball3.dy = 1
-ball1.time = -1000
 
-# Ball
-ball4 = turtle.Turtle()
-ball4.speed(0)
-ball4.shape("square")
-ball4.color("red")
-ball4.penup()
-ball4.goto(0, 0)
-ball4.dx = -1
-ball4.dy = 1
-ball1.time = -1000
+# # Ball
+# ball3 = turtle.Turtle()
+# ball3.speed(0)
+# ball3.shape("square")
+# ball3.color("yellow")
+# ball3.penup()
+# ball3.goto(0, 0)
+# ball3.dx = 0.10
+# ball3.dy = 0.10
 
-balls = [ball1, ball2, ball3, ball4]
+
+# # Ball
+# ball4 = turtle.Turtle()
+# ball4.speed(0)
+# ball4.shape("square")
+# ball4.color("red")
+# ball4.penup()
+# ball4.goto(0, 0)
+# ball4.dx = -0.10
+# ball4.dy = 0.10
+
+balls = [ball1]
 
 # Pen
 pen = turtle.Turtle()
@@ -157,14 +155,14 @@ while True:
             ball.dx *= -1
             os.system("afplay bounce.wav&")
 
-    # AI Player
-    closest_ball = balls[0]
-    for ball in balls:
-        if ball.xcor() > closest_ball.xcor():
-            closest_ball = ball
+    # # AI Player
+    # closest_ball = balls[0]
+    # for ball in balls:
+    #     if ball.xcor() > closest_ball.xcor():
+    #         closest_ball = ball
 
-    if paddle_b.ycor() < closest_ball.ycor() and abs(paddle_b.ycor() - closest_ball.ycor()) > 10:
-        paddle_b_up()
+    # if paddle_b.ycor() < closest_ball.ycor() and abs(paddle_b.ycor() - closest_ball.ycor()) > 10:
+    #     paddle_b_up()
 
-    elif paddle_b.ycor() > closest_ball.ycor() and abs(paddle_b.ycor() - closest_ball.ycor()) > 10:
-       paddle_b_down()
+    # elif paddle_b.ycor() > closest_ball.ycor() and abs(paddle_b.ycor() - closest_ball.ycor()) > 10:
+    #    paddle_b_down()

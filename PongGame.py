c0b06d890f538ce000d0c633d038d64284ab8bd5
= <=>
2024-12-26 18:07:10 +0700

    PongGame

--- PongGame.py
+++ PongGame.py
@@ -16,7 +16,7 @@ paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
 paddle_a.color("white")
-paddle_a.shapesize(stretch_wid=20, stretch_len=1)
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-350, 0)
 
